9f798cf5bb3b3e4ef38548280bc8485a7a31b3bd
Eugene Ichev <jeka_icheev@mail.ru>
2023-04-19 15:42:58 +0200

    airflow docker pgadmin

--- scripts/add_user.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-airflow users create \
-          --username admin \
-          --firstname admin \
-          --lastname admin \
-          --role Admin \
-          --email admin@example.org \
-          -p admin
